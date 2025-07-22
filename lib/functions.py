b368150b750c4ec945704f54c81f252d223bddaf
Brian Oduory <brianmubweka17@gmail.com>
2025-07-22 15:20:32 +0300

    creates functions in python

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    """
+    Takes no arguments and outputs "Hello, programmer!" to the terminal.
+    """
+    print("Hello, programmer!")
+
 
 def greet(name):
-    pass
+    """
+    Takes one argument (name) and outputs "Hello, name!" to the terminal.
+    """
+    print(f"Hello, {name}!")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    """
+    Takes one argument (name) with a default value of "programmer".
+    Outputs "Hello, name!" to the terminal.
+    If no argument is passed, outputs "Hello, programmer!".
+    """
+    print(f"Hello, {name}!")
+
 
 def add(num1, num2):
-    pass
+    """
+    Takes two numbers as arguments and returns their sum.
+    """
+    return num1 + num2
+
 
 def halve(number):
-    pass
+    """
+    Takes one number as an argument and returns that number divided by two.
+    """
+    return number / 2
